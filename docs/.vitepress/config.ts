7afd5e841d97e56592b8e6f56e7fd99ed3afe795
root <1749071150@qq.com>
2024-05-07 22:17:52 +0800

    文档整理

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -86,5 +86,8 @@ export default defineConfig({
     },
   },
   // github page base: '/项目名/'
-  base: '/cattle-blog/'
+  base: '/cattle-blog/',
+  head:[
+    ['link', { rel: 'icon', href: '/favicon.ico' }]
+  ]
 })
